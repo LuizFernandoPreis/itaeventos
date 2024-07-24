3c66dfa7fce2fd237635fd303ec4800aa2b9f3ab
LuizFernandoPreis <149117955+LuizFernandoPreis@users.noreply.github.com>
2024-07-23 23:41:50 -0300

    0.1

---
+++ app/(pages)/signin/page.tsx
@@ -0,0 +1,15 @@
+'use client'
+import Image from "next/image";
+import HomePage from "../../_components/HomePage";
+import { AuthProvider } from "../../contexts/AuthContext";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <AuthProvider>
+      <HomePage/>
+      </AuthProvider>
+
+      </main>
+  );
+}
--- middleware.ts
+++ middleware.ts
@@ -1,12 +1,13 @@
 import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
  
-// This function can be marked async if using await inside
+
 export function middleware(request: NextRequest) {
-  return NextResponse.redirect(new URL('/', request.url))
+  let cookie = request.cookies.get('nextauth.token')
+  if (!cookie) return NextResponse.redirect(new URL('/signin', request.url));
 }
  
 // See "Matching Paths" below to learn more
 export const config = {
-  matcher: ['/getTask'],
+  matcher: '/((?!signin|signup)):path*',
 }
