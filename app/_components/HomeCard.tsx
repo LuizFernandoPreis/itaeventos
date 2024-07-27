8b104f30b56bf8e14787079a6f179da7366c2de4
LuizFernandoPreis <149117955+LuizFernandoPreis@users.noreply.github.com>
2024-07-26 22:38:53 -0300

    1

--- app/_components/HomeCard.tsx
+++ app/_components/HomeCard.tsx
@@ -19,43 +19,42 @@ const HomeCard: React.FC<HomeCardProps> = ({ event }) => {
   
     return (
         <div 
-        className="max-w-sm rounded overflow-hidden shadow-lg cursor-pointer flex flex-col"
-        onClick={handleCardClick}
-      >
-        <div className="relative h-48 w-full bg-gray-200">
-          <Image 
-            src={event.image} 
-            alt={event.title} 
-            layout="fill" 
-            objectFit="cover" 
-            className="rounded-t"
-          />
-        </div>
-        <div className="px-6 py-4 flex-grow">
-          <div className="font-bold text-xl mb-2">{event.title}</div>
-          <p className="text-gray-700 text-base mb-2">
-            {event.date} - {event.location}
-          </p>
-          <p className="text-gray-700 text-base">
-            {event.description}
-          </p>
-        </div>
-        <div className="px-6 pt-4 pb-2 flex justify-between">
-          <button 
-            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-            onClick={(e) => { e.stopPropagation(); router.push(`/events/edit/${event.id}`); }}
-          >
-            Edit
-          </button>
-          <button 
-            className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded"
-            onClick={(e) => { e.stopPropagation(); alert('Delete functionality here'); }}
-          >
-            Delete
-          </button>
-        </div>
+      className="w-80 max-w-xs rounded overflow-hidden shadow-lg cursor-pointer flex flex-col"
+      onClick={handleCardClick}
+    >
+      <div className="relative h-48 bg-gray-200">
+        <Image 
+          src={event.image} 
+          alt={event.title} 
+          layout="fill" 
+          objectFit="cover" 
+          className="rounded-t"
+        />
       </div>
-    );
-  };
-
+      <div className="px-6 py-4 flex-grow">
+        <div className="font-bold text-xl mb-2">{event.title}</div>
+        <p className="text-gray-700 text-base mb-2">
+          {event.date} - {event.location}
+        </p>
+        <p className="text-gray-700 text-base">
+          {event.description}
+        </p>
+      </div>
+      <div className="px-6 pt-4 pb-2 flex justify-between">
+        <button 
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+          onClick={(e) => { e.stopPropagation(); router.push(`/events/edit/${event.id}`); }}
+        >
+          Edit
+        </button>
+        <button 
+          className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded"
+          onClick={(e) => { e.stopPropagation(); alert('Delete functionality here'); }}
+        >
+          Delete
+        </button>
+      </div>
+    </div>
+  );
+};
 export default HomeCard;
